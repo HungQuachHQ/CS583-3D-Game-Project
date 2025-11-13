306f926397f4d3a3e50fc44859df68bc0da904c3
Hung Quach <161780888+HungQuachHQ@users.noreply.github.com>
2025-11-12 20:16:15 -0800

    Simple Start Menu
    - Implemented a simple start menu.

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour {
+    public void OnStartClick() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnQuitClick() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
